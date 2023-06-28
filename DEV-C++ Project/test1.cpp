6208df558444e5e85b92f5bae1db9abc43b94ff0
HG-Freedom <137777363+HG-Freedom@users.noreply.github.com>
2023-06-28 15:21:53 +0800

    Update test1.cpp

--- DEV-C++ Project/test1.cpp
+++ DEV-C++ Project/test1.cpp
@@ -1,6 +1,18 @@
 #include"stdio.h"
 int main()
 {
-	printf("Hello Word!\n");
+	int a[5],i,j,t;
+	printf("Please Input 5 numbers\n");
+	for(i=0;i<5;i++)
+		scanf("%d",&a[i]);
+	for(j=3;j>=0;j--)
+	{
+	for(i=0;i<=j;i++)
+	if(a[i]>a[i+1])
+	{t=a[i];a[i]=a[i+1];a[i+1]=t;}
+	}
+	for(i=0;i<5;i++)
+	printf("%d ",a[i]);
+	printf("\nHello Word!\n");
 	return 0;
 }
